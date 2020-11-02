e67e74e4b8883d7877362f8e1b4a00e1e3280189
Anthony Caamano <73439902+aac95@users.noreply.github.com>
2020-11-02 13:49:37 -0500

    Update push_button.ino

--- Adruino_code/push_button.ino
+++ Adruino_code/push_button.ino
@@ -1,18 +1,30 @@
-// this is just the push button function, will be put into the  
+// this is just the push button function test to verify my code works as intended 
+// NOTE: for this code, the button connects high and there is a 100 ohm to ground on the 7 pin
 unsigned long startedWaiting;
 unsigned long wait_time; // this will be passed in most likely, just the calced delay
 void setup() {
   pinMode(7, INPUT);  
+  pinMode(8, OUTPUT); // pass condition indicator
+  pinMode(4, OUTPUT); // fail condition indicator
 }
 
-bool loop() {
+// the code here runs to false either way after the wait time (3 sec)
+// in the real code it will return after pass and fail
+// the point of this is to test the timer method and see if pressing the button within the timw
+// gets us to out pass condition
+void loop() {
+  wait_time = 3000;
   startedWaiting = millis();
-  while(millis() - startedWaiting <= wait_time) // while wait_time ammount of time hasn't passed
+  while(millis() - startedWaiting <= wait_time)
   {
-    if (digitalRead(7)==HIGH) // if button pushed
+    if (digitalRead(7)==HIGH)
     {
-       return true; // return true (they passed)
+       //return true;
+       digitalWrite(8, HIGH);
     }
   }
-  return false; // if the time passes, they fail
-}
+  //return false;  
+  digitalWrite(4, HIGH);
+  delay(3000); 
+  digitalWrite(8, LOW);
+  digitalWrite(4, LOW);
